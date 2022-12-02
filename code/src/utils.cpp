356dc7ba46a095807a74a0b46cf8dbd46af02ddd
Rhuvy <eloi.martaille.richard@umontreal.ca>
2022-12-02 11:52:13 +0100

    one step implemented

--- code/src/utils.cpp
+++ code/src/utils.cpp
@@ -12,3 +12,16 @@ void make_diag(int M, double h, double dt, arma::cx_mat){
 
     }
 }
+
+// this function solves the linear matrix equation to find the n+1 step
+arma::vec one_step(arma::mat A, arma::mat B, arma::vec u_n){
+    
+    // First step 
+    arma::vec b = B * u_n;
+    
+    // Now we solve the system with arma::solve, can implement own function later
+    arma::vec u_n1;
+    u_n1 = arma::solve(A, b);
+
+    return u_n1;
+}
