5da6f729ec0d3c02dfec0ad25c06c0e401b9cd3e
christopheblomsen <christophe.blomsen@gmail.com>
2022-12-08 11:27:50 +0100

    added potential file

--- code/main.cpp
+++ code/main.cpp
@@ -5,29 +5,41 @@
 
 int main(int argc, char *argv[]) {
 
-    int M = 5;
-    double h = 0.005;
-    double dt = 2.5e-5;
+    arma::mat V;
+    std::cout << argc << std::endl;
+    if (argc == 2){
+            std::string filename = argv[1];
+            V.load(arma::csv_name(filename));
+    }
+    else{
+        V = arma::mat(3, 3, arma::fill::eye);
+    }
 
-    double xc = 0.25;
-    double x = 0;
-    double sigma_x = 0.05;
-    double px = 200;
+    std::cout << V << std::endl;
 
-    double yc = 0.5;
-    double y = 0;
-    double sigma_y = 0.05;
-    double py = 0;
+    // int M = 5;
+    // double h = 0.005;
+    // double dt = 2.5e-5;
 
-    PDESolver test = PDESolver(M, h, dt);
+    // double xc = 0.25;
+    // double x = 0;
+    // double sigma_x = 0.05;
+    // double px = 200;
 
-    test.make_mat();
-    arma::cx_mat U = test.create_u_mat(xc, yc, x, y, sigma_x, sigma_y, px, py);
+    // double yc = 0.5;
+    // double y = 0;
+    // double sigma_y = 0.05;
+    // double py = 0;
 
-    test.normalized_U(U);
+    // PDESolver test = PDESolver(M, h, dt);
 
-    std::cout << std::endl;
-    std::cout << U << std::endl;
+    // test.make_mat();
+    // arma::cx_mat U = test.create_u_mat(xc, yc, x, y, sigma_x, sigma_y, px, py);
+
+    // test.normalized_U(U);
+
+    // std::cout << std::endl;
+    // std::cout << U << std::endl;
 
     // std::cout << test.A << std::endl;
     // std::cout << test.B << std::endl;
