5a6a0de39843dba881cc7c2f826a0ad031b0a58e
Rhuvy <eloi.martaille.richard@umontreal.ca>
2022-12-12 18:48:22 +0100

    py

--- code/prob7.py
+++ code/prob7.py
@@ -9,8 +9,8 @@ import os
 os.chdir(r'/home/rhuvy/Documents/FYS4150/Projects/fys4150_pro5/code')
 
 # load data
-data = pa.cx_mat()
-data.load('test_good.bin')
+data = pa.cx_cube()
+data.load('sim.bin')
 data = np.asarray(data)
 
 
@@ -26,11 +26,11 @@ time = np.arange(0, T, dt)
 
 prob = np.abs(data)**2
 print(data.shape)
-tot_prob = np.sum(prob, axis=0)
+# tot_prob = np.sum(prob, axis=0)
 
 # prob = np.conjugate(data)*data
 # tot_prob = np.real(np.sum(prob, axis=0))
-print(np.abs(tot_prob -1))
+# print(np.abs(tot_prob -1))
 # plt.figure()
 # plt.plot(time, tot_prob-1)
 # plt.xlabel('Time')
@@ -40,15 +40,15 @@ print(np.abs(tot_prob -1))
 # plt.show()
 # print(data[:10, 0])
 
-test = np.empty(shape=(N, N, 320))
+# test = np.empty(shape=(N, N, 320))
 
-for i in range(320):
-    data[:N-1,i] = test[:N-1, 0, i]
-    data
-data = data.reshape(N,N, 320)
-prob = np.abs(data)**2
+# for i in range(320):
+#     data[:N-1,i] = test[:N-1, 0, i]
+#     data
+# data = data.reshape(N,N, 320)
+# prob = np.abs(data)**2
 
 # initial condition 
 plt.figure()
-im = plt.imshow(prob[:,:,0])
+im = plt.imshow(prob[319,:,:])
 plt.show()
