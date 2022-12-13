c2b6c96effee90952e3ed4a191d4c3738b2f7d89
christopheblomsen <christophe.blomsen@gmail.com>
2022-12-13 22:27:58 +0100

    more robust shelling

--- code/shell_scripts/animate.sh
+++ code/shell_scripts/animate.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/sh
 PYTHON_PATH=python
 DATA_PATH=data
-echo $PYTHON_PATH
-for file in $DATA_PATH/*.bin
+ANIMATION="animation/"
+
+if [ ! -d "$ANIMATION" ]; then
+    echo "Directory not found and will be created"
+    mkdir $ANIMATION
+else
+    echo "Directory found"
+fi
+
+for FILE in $DATA_PATH/*.bin
 do
-    echo "Animating " $file
-    save=$(basename -s '.bin' $file)
-    python3 $PYTHON_PATH/anim.py $file $save
+    echo "Animating " $FILE
+    SAVE=$(basename -s '.bin' $FILE)
+    python3 $PYTHON_PATH/anim.py $FILE $SAVE
+    mv $(echo $SAVE.mp4) $ANIMATION
 done
--- code/shell_scripts/run_all.sh
+++ code/shell_scripts/run_all.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/sh
 INPUT=input_files
+DATA="data/"
 
-for file in $INPUT/*csv
+if [ ! -d "$DATA" ]; then
+    echo "Directory not found and will be created"
+    mkdir $DATA
+else
+    echo "Directory found"
+fi
+
+mv $INPUT/double* $INPUT/no* $INPUT/single* $INPUT/triple* .
+
+for FILE in $INPUT/problem*csv
 do
-    echo "Simulating " $file
-    ./main $file
+    echo "Simulating " $FILE
+    BIN=$(echo $(basename -s '.csv' $FILE).bin)
+    ./main $FILE
+    mv $BIN $DATA
 done
+
+mv *slit*csv $INPUT
