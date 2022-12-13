6bd4a4c369176bb5e5e7cdb6edb1c3d1eef3b8d6
Rhuvy <eloi.martaille.richard@umontreal.ca>
2022-12-13 13:00:53 +0100

    problem7 done

--- code/prob7.py
+++ code/prob7.py
@@ -4,51 +4,82 @@ import pyarma as pa
 import os 
 
 
+# Figures settings
+plt.rc('text', usetex=True)
+# plt.rc('font', family='Helvetica')
+
+SMALL_SIZE = 15
+MEDIUM_SIZE = 18
+BIGGER_SIZE = 22
+
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
 
 # directory of the data
-os.chdir(r'/home/rhuvy/Documents/FYS4150/Projects/fys4150_pro5/code')
+os.chdir(r'./data')
+
+
+
+def get_data(problem):
+    """
+    Loads the binary file and converts it
+    to numpy array
+
+    Parameters:
+    -----------
+    problem:
+            binary file created in cpp
+
+    Returns:
+            numpy array
+    """
+    data = pa.cx_mat()
+    data.load(problem, pa.arma_binary)
+    U = np.array(data, dtype=np.clongdouble)
+    return U
+
 
 # load data
-data = pa.cx_cube()
-data.load('sim.bin')
-data = np.asarray(data)
+data_no_slit = get_data('problem7_no_slit.bin')
+data_slit = get_data('problem7_double_slit.bin')
 
+print(data_no_slit.shape)
+print(data_slit.shape)
 
+# Time parameters 
 T = 0.008
 dt = 2.5e-5
+time = np.arange(0, T, dt)
 
 M = int(1/ 0.005) + 1
 N = M - 2
 L = N*N
 
+# probablity
+prob_no_slit = np.abs(data_no_slit)**2
+prob_slit = np.abs(data_slit)**2
 
-time = np.arange(0, T, dt)
+# total probability
+tot_prob_no_slit = np.sum(prob_no_slit, axis=0)
+tot_prob_slit = np.sum(prob_slit, axis=0)
 
-prob = np.abs(data)**2
-print(data.shape)
-# tot_prob = np.sum(prob, axis=0)
+# absolute error 
+abs_error = np.abs(tot_prob_no_slit - 1)
+abs_error_slit = np.abs(tot_prob_slit - 1)
 
-# prob = np.conjugate(data)*data
-# tot_prob = np.real(np.sum(prob, axis=0))
-# print(np.abs(tot_prob -1))
-# plt.figure()
-# plt.plot(time, tot_prob-1)
-# plt.xlabel('Time')
-# plt.yticks()
-# # plt.ylim(np.min(tot_prob), np.max(tot_prob))
-# plt.title('Probability of finding the particle in the box')
-# plt.show()
-# print(data[:10, 0])
-
-# test = np.empty(shape=(N, N, 320))
-
-# for i in range(320):
-#     data[:N-1,i] = test[:N-1, 0, i]
-#     data
-# data = data.reshape(N,N, 320)
-# prob = np.abs(data)**2
-
-# initial condition 
+# plot the absolute error
 plt.figure()
-im = plt.imshow(prob[319,:,:])
+plt.plot(time, abs_error, label='No slit')
+plt.plot(time, abs_error_slit, label='Double slit')
+plt.xlabel('Time')
+plt.ylabel('Absolute error')
+plt.title('Absolute error in the total probability')
+plt.legend()
+# plt.savefig('problem7_error.pdf')
 plt.show()
